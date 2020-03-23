01da77d65c59d42e350e6060d847d2e3c149c30b
Your Name <you@example.com>
2020-03-23 09:05:15 +0800

    send_email_code signup

--- src/signup/index.tsx
+++ src/signup/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from "axios";
+
+import * as qs from 'querystring';
 
 import {
     Form,
@@ -8,13 +11,43 @@ import {
     Col
 } from 'antd';
 
-class Signup extends React.Component {
+const ENDPOINT = "http://58b5dd3da8514f30a8dfbf42bb0a740c-cn-beijing.alicloudapi.com";
+const send_email_code_url = `${ENDPOINT}/send-email-code`;
+const signup_url = `${ENDPOINT}/signup`;
+
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+
+type Props = {};
+type State = { email_property: string, token_property: string };
+
+class Signup extends React.Component<Props, State> {
     constructor(props: any) {
         super(props)
+        this.state = { email_property: '', token_property: '' };
     }
 
-    onFinish(values: any) {
-        console.log('Success:', values);
+    send_email_code = async () => {
+        const body = qs.stringify({
+            'email': this.state.email_property,
+        })
+        const resp = await axios.post(send_email_code_url, body, { headers })
+        this.setState({ token_property: resp.data.token })
+    }
+
+    onEmailChange = (e: any) => {
+        this.setState({ email_property: e.target.value })
+    }
+
+    onFinish = async (values: any) => {
+        const body = qs.stringify(values)
+        const signup_headers = {
+            ...headers,
+            'X-Token': this.state.token_property
+        }
+        const resp = await axios.post(signup_url, body, { headers: signup_headers })
+        localStorage.setItem("token", resp.data.token);
     }
 
     onFinishFailed(values: any) {
@@ -25,7 +58,6 @@ class Signup extends React.Component {
         return (
             <Form
                 name="basic"
-                initialValues={{ remember: true }}
                 onFinish={this.onFinish}
                 onFinishFailed={this.onFinishFailed}
             >
@@ -38,40 +70,40 @@ class Signup extends React.Component {
                         },
                         {
                             required: true,
-                            message: 'Please input your E-mail!',
+                            message: '邮箱不能为空',
                         },
                     ]}
                 >
-                    <Input placeholder="输入邮箱，作为登陆账号" />
+                    <Input onChange={this.onEmailChange} placeholder="输入邮箱，作为登陆账号" />
                 </Form.Item>
 
                 <Form.Item
                     name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
+                    rules={[{ required: true, message: '密码不能为空' }]}
                 >
-                    <Input.Password />
+                    <Input.Password placeholder="输入账号密码" />
                 </Form.Item>
 
                 <Form.Item>
                     <Row>
                         <Col span={20}>
                             <Form.Item
-                                name="captcha"
+                                name="code"
                                 noStyle
-                                rules={[{ required: true, message: 'Please input the captcha you got!' }]}
+                                rules={[{ required: true, message: '验证码不能为空' }]}
                             >
-                                <Input />
+                                <Input placeholder="输入邮件验证码" />
                             </Form.Item>
                         </Col>
                         <Col offset={1} span={3}>
-                            <Button block>Get</Button>
+                            <Button onClick={this.send_email_code} block>获取验证码</Button>
                         </Col>
                     </Row>
                 </Form.Item>
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
-                        Submit
+                        下一步
                     </Button>
                 </Form.Item>
             </Form>
