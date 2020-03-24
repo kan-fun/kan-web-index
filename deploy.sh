6ce13077893fa9727a53673e6566c5a51c792030
Your Name <you@example.com>
2020-03-24 15:51:46 +0800

    Fix bug in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
 chmod 755 ossutil64
 ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
 ./ossutil64 rm -rf oss://kan-web-index/
-./ossutil64 cp -rf ./dist/ --include "*"  oss://kan-web-index/
+./ossutil64 cp -rf ./build/ --include "*"  oss://kan-web-index/
