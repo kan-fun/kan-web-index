a6ba62bc7a09723c1133b307ea461f30b0d3f7f5
Your Name <you@example.com>
2020-03-24 15:48:02 +0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
+chmod 755 ossutil64
+./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
+./ossutil64 rm -rf oss://kan-web-index/
+./ossutil64 cp -rf ./dist/ --include "*"  oss://kan-web-index/
