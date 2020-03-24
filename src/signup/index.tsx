d21c509c1556c803820c4780e20512ca617e6f8c
Your Name <you@example.com>
2020-03-24 18:21:09 +0800

    Add Landing

--- src/signup/index.tsx
+++ src/signup/index.tsx
@@ -21,15 +21,49 @@ const headers = {
 }
 
 type Props = {};
-type State = { email_property: string, token_property: string, error_property: boolean };
+type State = {
+    email_property: string,
+    token_property: string,
+    error_property: boolean,
+    is_waiting_for_code: boolean,
+    code_time_count: number,
+    timer: any
+};
 
 class Signup extends React.Component<Props, State> {
     constructor(props: any) {
         super(props)
-        this.state = { email_property: '', token_property: '', error_property: false };
+        this.state = {
+            email_property: '',
+            token_property: '',
+            error_property: false,
+            is_waiting_for_code: false,
+            code_time_count: 0,
+            timer: null
+        };
     }
 
     send_email_code = async () => {
+        if (!this.state.timer) {
+            this.setState({
+                code_time_count: 60,
+                is_waiting_for_code: true,
+                timer: setInterval(() => {
+                    if (this.state.code_time_count > 0) {
+                        this.setState({
+                            code_time_count: this.state.code_time_count - 1
+                        })
+                    } else {
+                        clearInterval(this.state.timer);
+                        this.setState({
+                            is_waiting_for_code: false,
+                            timer: null
+                        })
+                    }
+                }, 1000)
+            })
+        }
+
         const body = qs.stringify({
             'email': this.state.email_property,
         })
@@ -83,37 +117,41 @@ class Signup extends React.Component<Props, State> {
                             },
                         ]}
                     >
-                        <Input onChange={this.onEmailChange} placeholder="输入邮箱，作为登陆账号" />
+                        <Input size="large" onChange={this.onEmailChange} placeholder="输入邮箱，作为登陆账号" />
                     </Form.Item>
 
                     <Form.Item
                         name="password"
                         rules={[{ required: true, message: '密码不能为空' }]}
                     >
-                        <Input.Password placeholder="输入账号密码" />
+                        <Input.Password size="large" placeholder="输入账号密码" />
                     </Form.Item>
 
                     <Form.Item>
                         <Row>
-                            <Col span={20}>
+                            <Col span={15}>
                                 <Form.Item
                                     name="code"
                                     noStyle
                                     rules={[{ required: true, message: '验证码不能为空' }]}
                                 >
-                                    <Input placeholder="输入邮件验证码" />
+                                    <Input size="large" placeholder="输入邮件验证码" />
                                 </Form.Item>
                             </Col>
-                            <Col offset={1} span={3}>
-                                <Button onClick={this.send_email_code} block>获取验证码</Button>
+                            <Col offset={1} span={8}>
+                                <Button onClick={this.send_email_code} disabled={this.state.is_waiting_for_code} size="large" block>
+                                    {this.state.is_waiting_for_code ? `${this.state.code_time_count}s` : "获取验证码"}
+                                </Button>
                             </Col>
                         </Row>
                     </Form.Item>
 
+                    <br />
+
                     <Form.Item>
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" shape="round" size="large" block>
                             下一步
-                    </Button>
+                        </Button>
                     </Form.Item>
                 </Form>
             </div>
